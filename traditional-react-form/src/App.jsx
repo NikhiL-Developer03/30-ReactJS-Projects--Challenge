7c3464fcf2da86a071ddc1a649f8f9a7027e9919
Nikhil.Developer03 <nikhil.developer03@gmail.com>
2024-10-04 23:19:40 +0530

    add new project called - traditional-react-form

---
+++ traditional-react-form/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TraditionalForm from './components/traditional-form'
+function App() {
+
+  return (
+    <>
+    <TraditionalForm />
+    </>
+  )
+}
+
+export default App
